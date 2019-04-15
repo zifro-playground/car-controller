11a2731cfc2453082cae0dc380fa533c33c2a9ec
kalle (jag) <kalle.jillheden@gmail.com>
2019-04-15 10:13:53 +0200

    Added AssemblyInfo for car-controller, starting at v1.0.0

---
+++ Assets/CarController/Scripts/CarControllerAssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
+
+public class CarControllerAssemblyInfo : PM.AssemblyInfo
+{
+	public override bool canBeEdited => true;
+}
