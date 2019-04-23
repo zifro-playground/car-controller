1c7ed56d64314a7c115296d07bb2ffee1ba98ee0
kalle (jag) <kalle.jillheden@gmail.com>
2019-04-23 21:46:06 +0200

    Upped version to 1.1.0

--- Assets/CarController/Scripts/CarControllerAssemblyInfo.cs
+++ Assets/CarController/Scripts/CarControllerAssemblyInfo.cs
@@ -1,11 +1,11 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
 using UnityEngine;
 
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyVersion("1.1.0")]
+[assembly: AssemblyFileVersion("1.1.0")]
+[assembly: AssemblyInformationalVersion("1.1.0")]
 
 public class CarControllerAssemblyInfo : PM.AssemblyInfo
 {
