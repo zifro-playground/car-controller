0e94b874219a0d88e7b539df8395ce2d30a83680
kalle (jag) <kalle.jillheden@gmail.com>
2019-04-23 21:45:16 +0200

    Added playmode unit tests

---
+++ Assets/CarController.Tests.PlayMode/PlayThroughSampleTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Linq;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+using ZifroPlaygroundTests;
+using ZifroPlaygroundTests.PlayMode;
+
+namespace CarController.Tests.PlayMode
+{
+	public class PlayThroughSample : PlayThroughLevelsTests
+	{
+		static CaseTestData[] GetActiveCases()
+		{
+			return PlaygroundTestHelper.GetActiveCases("car-controller");
+		}
+
+		static LevelTestData[] GetActiveLevels()
+		{
+			return PlaygroundTestHelper.GetActiveLevels("car-controller");
+		}
+
+		protected override string testingScenePath => "Assets/CarController.Tests.PlayMode/MainSceneForTesting.unity";
+
+		[UnityTest]
+		public override IEnumerator TestPlayWholeGame()
+		{
+			return TestPlayWholeGame(GetActiveLevels());
+		}
+
+		[UnityTest]
+		public override IEnumerator TestPlayGuidesInLevel([ValueSource(nameof(GetActiveLevels))] LevelTestData data)
+		{
+			return base.TestPlayGuidesInLevel(data);
+		}
+		
+		[UnityTest]
+		public override IEnumerator TestPlayLevel([ValueSource(nameof(GetActiveLevels))] LevelTestData data)
+		{
+			return base.TestPlayLevel(data);
+		}
+	}
+}
